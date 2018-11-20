93f3e9cc692d42b6f9b50b61a2dc53234a70e08d
darcy97 <darcy97zh@gmail.com>
2018-11-20 18:50:06 +0800

    导入boss素材，添加ai追踪主主角

--- Assets/Scripts/MonAIController.cs
+++ Assets/Scripts/MonAIController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class MonAIController : MonoBehaviour {
 
-    public Transform target;
+    public Transform target;  //AI 目标
+    private CharacterController m_characterController;
+
+    private Vector3 target_posion;
+
+    const float speed = 1f;
 
 
-
-	// Use this for initialization
 	void Start () {
-		
+
+        m_characterController = GetComponent<CharacterController>();
+        
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+
+        
+
 	}
+
+
+
+    private void FixedUpdate()
+    {
+        target_posion = target.position;
+
+        Vector3 direction = Time.deltaTime * speed * (target.position - transform.position).normalized;
+        direction.y = 0;
+
+        m_characterController.Move(direction*Time.fixedDeltaTime*speed);
+    }
 }
