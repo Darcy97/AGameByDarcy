7761450ef6ae3526d5f78fd5835524aacfb73f1e
darcy97 <darcy97zh@gmail.com>
2018-11-30 13:20:51 +0800

    add input compoent

---
+++ Assets/Sources/Components/Input/InputComponents.cs
@@ -0,0 +1,11 @@
+﻿using Entitas;
+using Entitas.CodeGeneration.Attributes;
+
+[Input][Unique]
+public sealed class InputMouseDownComponent : IComponent {}
+
+[Input][Unique]
+public sealed class InputKeyComponent : IComponent
+{
+    public string value;
+}
--- Assets/Sources/Generated/Input/InputComponentsLookup.cs
+++ Assets/Sources/Generated/Input/InputComponentsLookup.cs
@@ -8,15 +8,18 @@
 //------------------------------------------------------------------------------
 public static class InputComponentsLookup {
 
+    public const int InputKey = 0;
+    public const int InputMouseDown = 1;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 2;
 
     public static readonly string[] componentNames = {
-
+        "InputKey",
+        "InputMouseDown"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(InputKeyComponent),
+        typeof(InputMouseDownComponent)
     };
 }
